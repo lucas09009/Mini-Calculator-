4ae9f34733179584ddacbead50705543141314ea
lucas09009 <luckpegan@gmail.com>
2025-08-12 13:34:14 +0000

    Mis-à-jour de mok fichier

--- Assignment_of_week3_python_calculatrice.py
+++ Assignment_of_week3_python_calculatrice.py
@@ -1,1 +1,14 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 0.2 :
+     definitive_price = price - (price* discount_percent)
+     
+    else:
+        definitive_price = price
+        
+    return definitive_price
 
+price = float(input(" veuillez renseigner le prix de l'article: "))
+discount_percent = float(input("veuillez indiquer le pourcentage de ma remise sur une echelle de 0% à 100%: "));
+discount_percent  = discount_percent / 100
+result = calculate_discount(price, discount_percent );
+print("Le prix final est", result)
